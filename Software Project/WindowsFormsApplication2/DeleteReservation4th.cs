a33c22a1e942c82664b93281bda6068c9f98384b
HarisA92 <harisaljovic92@hotmail.com>
2017-05-23 23:11:08 +0200

    Update DeleteReservation4th.cs

--- Software Project/WindowsFormsApplication2/DeleteReservation4th.cs
+++ Software Project/WindowsFormsApplication2/DeleteReservation4th.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using MySql.Data.MySqlClient;
-
+//Haris Aljovic part
 namespace WindowsFormsApplication2
 {
     public partial class DeleteReservation4th : Form
